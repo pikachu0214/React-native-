52bfbd11129bfa657df859f65aca05fe55998792
Danny Yen <danny.y.yen@gmail.com>
2018-09-11 21:32:50 +0300

    updated addButton in Todo

---
+++ myapp/src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: '', 
+      description: '', 
+      todos: []
+    }
+  }
+  inputChanged = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+  deleteTodos = (item, index) => {
+    const deletetodos = Object.assign([], this.state.todos);
+    deletetodos.splice(item, 1);
+    this.setState({todos: []});
+  }
+
+  addTodo = (event) => {
+    event.preventDefault();
+    const listObject = {description: this.state.description, date: this.state.date}
+    this.setState({
+      todos: [listObject, ...this.state.todos]
+    });
+  
+  }
+  render() {
+    const todolist = this.state.todos.map((item, index) => <tr key={index}>
+        <td>{item.date}</td>
+        <td>{item.description}</td>
+      </tr>);
+    return <div className="App">
+        <div className="App-header">
+          <h2>Simple Todolist</h2>
+        </div>
+        <div>
+          <form onSubmit={this.addTodo}>
+            <fieldset>
+              <legend>Add todo:</legend>
+              Description:
+              <input type="text" id="#"name="description" onChange={this.inputChanged} value={this.state.description} />
+              Date:
+              <input type="date" name="date" onChange={this.inputChanged} value={this.state.date} />
+              <input type="submit" value="Add" />
+            </fieldset>
+          </form>
+        </div>
+        <div>
+          <table className='App'>
+            <tbody>
+              <tr>
+                <th>Date</th>
+                <th>Description</th>
+              </tr>
+              {todolist}
+            </tbody>
+          </table>
+        </div>
+      </div>;
+  }
+}
+export default App;
