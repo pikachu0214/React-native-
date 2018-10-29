5bfb95a69a65ed93291e816f947a9651160130dd
Danny Yen <danny.y.yen@gmail.com>
2018-10-29 10:28:00 +0200

    currency converter with picker

--- currencyconverter/App.js
+++ currencyconverter/App.js
@@ -5,65 +5,71 @@ import {
   View,
   Button,
   TextInput,
-  FlatList,
   Alert,
-  StatusBar,
-  Image,
-  Picker,
-  Item,
-  ScrollView
+  Picker
 } from "react-native";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      currency: [],
-      amount: 0,
-      picker: "",
-      pickIndex: 0,
-      total: 0
-    };
+    this.state = { currencies: [], rates: {}, value: 0, result: 0 };
   }
   componentDidMount() {
-    this.getJobs();
+    this.getData();
   }
-  getJobs = () => {
-    const url = "http://data.fixer.io/api/latest?access_key=ad25939469631d79ef81cce2d1ba2d06&format=1";
-    fetch(url)
+  getData = () => {
+       fetch("http://data.fixer.io/api/latest?access_key=ad25939469631d79ef81cce2d1ba2d06&format=1")
       .then(response => response.json())
       .then(responseJson => {
-        this.setState({ currency: responseJson.rates });
+        this.setState({ rates: responseJson.rates });
+        this.setState({ currencies: Object.keys(this.state.rates) });
       })
       .catch(error => {
         Alert.alert(error);
       });
-  };
-  listSeparator = () => {
-    return (
-      <View
-        style={{
-          height: 1,
-          width: "80%",
-          backgroundColor: "#CED0CE",
-          marginLeft: "10%"
-        }}
-      />
-    );
+  }
+  convert = () => {
+    if (this.state.currency != undefined) {
+      let rateValue = this.state.rates[this.state.currency];
+      let userValue = this.state.value;
+      let result = userValue / rateValue;
+
+      this.setState({ result: result.toFixed(2) });
+    }
+    else {
+      Alert.alert("Choose currency");
+    }
   };
   render() {
+    const labels = this.state.currencies.map(currency => (
+      <Picker.Item key={currency} label={currency} value={currency} />
+    ));
     return (
       <View style={styles.container}>
-        <FlatList data={this.state.currency}
-          ItemSeparatorComponent={this.listSeparator}
-          style={{ marginLeft: "5%" }}
-          keyExtractor={(x, i) => i}
-          renderItem={({ item }) => (
-            <View>
-              <Item {...item} />
-            </View>
-          )}
-        />
+        <Text style={{ fontSize: 30, marginBottom: "20%", color: "blue" }}>
+          Currency Converter
+        </Text>
+        <Text style={styles.textResult}>{this.state.result} &euro;</Text>
+
+        <View style={styles.userInput}>
+          <TextInput
+            style={{ width: 100, fontSize: 20, margin: 20 }}
+            keyboardType="numeric"
+            onChangeText={value => this.setState({ value })}
+          />
+          <Picker
+            selectedValue={this.state.currency}
+            style={{ height: 50, width: 100 }}
+            onValueChange={(itemValue, itemIndex) =>
+              this.setState({ currency: itemValue })
+            }
+          >
+            <Picker.Item label=" " />
+            {labels}
+          </Picker>
+        </View>
+
+        <Button title="Convert" onPress={this.convert} />
       </View>
     );
   }
